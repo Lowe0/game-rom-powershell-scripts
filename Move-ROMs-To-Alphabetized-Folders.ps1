2be0118650a1c229b2876ef24d33a86a53ed3a00
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2023-01-02 19:47:03 -0500

    Rework to use a hashtable

--- Move-ROMs-To-Alphabetized-Folders.ps1
+++ Move-ROMs-To-Alphabetized-Folders.ps1
@@ -20,7 +20,7 @@ None. This script does not generate any output.
 
 .EXAMPLE
 
-C:\PS> .\Set-ROMs-Read-Only.ps1 -romPath C:\emulator\roms
+C:\PS> .\Set-ROMs-Read-Only.ps1 -romPath C:\RetroArch\roms
 
 #>
 
@@ -30,7 +30,44 @@ param(
 )
 
 [int] $totalMoved = 0
-[string] $numericPath = "0-9"
+$folderMap = @{}
+
+$folderMap.Add("0","0-9")
+$folderMap.Add("1","0-9")
+$folderMap.Add("2","0-9")
+$folderMap.Add("3","0-9")
+$folderMap.Add("4","0-9")
+$folderMap.Add("5","0-9")
+$folderMap.Add("6","0-9")
+$folderMap.Add("7","0-9")
+$folderMap.Add("8","0-9")
+$folderMap.Add("9","0-9")
+$folderMap.Add("A","A-C")
+$folderMap.Add("B","A-C")
+$folderMap.Add("C","A-C")
+$folderMap.Add("D","D-F")
+$folderMap.Add("E","D-F")
+$folderMap.Add("F","D-F")
+$folderMap.Add("G","G-I")
+$folderMap.Add("H","G-I")
+$folderMap.Add("I","G-I")
+$folderMap.Add("J","J-L")
+$folderMap.Add("K","K-L")
+$folderMap.Add("L","K-L")
+$folderMap.Add("M","M-O")
+$folderMap.Add("N","M-O")
+$folderMap.Add("O","M-O")
+$folderMap.Add("P","P-S")
+$folderMap.Add("Q","P-S")
+$folderMap.Add("R","P-S")
+$folderMap.Add("S","P-S")
+$folderMap.Add("T","T-V")
+$folderMap.Add("U","T-V")
+$folderMap.Add("V","T-V")
+$folderMap.Add("W","W-Z")
+$folderMap.Add("X","W-Z")
+$folderMap.Add("Y","W-Z")
+$folderMap.Add("Z","W-Z")
 
 [string] $romPathAbsolute = (Resolve-Path $romPath).Path
 
@@ -42,14 +79,13 @@ if (!(Test-Path $romPath)) {
 $romItems = Get-ChildItem -Path $romPath -File -Recurse
 
 foreach ($romFile in $romItems){
-    $firstChar = $romFile.Name.Substring(0,1)
+    $fileName = $romFile.Name
+    $firstChar = $fileName.Substring(0,1)
     $targetPath = $romPathAbsolute
-    if ($firstChar -match "^\d$") { 
-        $targetPath = $targetPath + '\' + $numericPath
-    } elseif ($firstChar -match "^\w$") {
-        $targetPath = $targetPath + '\' + $firstChar
+    if ($folderMap.ContainsKey($firstChar)) {
+        $targetPath = $targetPath + '\' + $folderMap[$firstChar]
     } else {
-        Write-Host "Skipping File"
+        Write-Host "Skipping $fileName"
         continue
     }
     $pathExpected = $targetPath + '\' + $romFile.Name
@@ -58,7 +94,6 @@ foreach ($romFile in $romItems){
         if (!(Test-Path $targetPath)) {
             New-Item -Path $targetPath -ItemType Directory | Out-Null
         }
-        $fileName = $romFile.Name
         Write-Host "Moving $fileName..."
         Move-Item -Path $pathActual -Destination $targetPath
         $totalMoved++
